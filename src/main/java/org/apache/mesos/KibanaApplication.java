55ff7ccc89ec4810812b9ba205145c7c03f8d5bf
Phil Winder <phil@winderresearch.com>
2016-03-04 13:33:40 +0000

    Initial mesos-starter commit. Use commit version. Working in bridge mode.

---
+++ src/main/java/org/apache/mesos/KibanaApplication.java
@@ -0,0 +1,12 @@
+package org.apache.mesos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KibanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KibanaApplication.class, args);
+    }
+}
---
+++ src/test/java/org/apache/mesos/KibanaApplicationTests.java
@@ -0,0 +1,11 @@
+package org.apache.mesos;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = KibanaApplication.class)
+//public class KibanaApplicationTests {
+//
+////	@Test
+////	public void contextLoads() {
+////	}
+//
+//}
